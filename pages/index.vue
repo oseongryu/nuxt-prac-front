b3034ff66f3dd74fb89aefd5dad9262fd4c7345c
oseongryu <oseongryu@gmail.com>
2023-02-01 14:24:18 +0900

    chore: clean

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>{{ name }}</h1>
     <!-- <Tutorial /> -->
+    <NuxtLogo />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Ref, Prop, PropSync, Emit, Watch, Model, Inject, InjectReactive, Provide, ProvideReactive, Vue } from 'vue-property-decorator'
 import PracAppComponent from '@/components/prac-app-component'
+import NuxtLogo from '~/components/NuxtLogo.vue'
 
 @Component({
   components: {}
