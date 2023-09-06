a96e3f020a7bc6108e439109d2a5a7590da1d55d
oseongryu <oseongryu@gmail.com>
2023-09-06 17:16:00 +0900

    feat: automation

--- pages/common/automation-index.vue
+++ pages/common/automation-index.vue
@@ -3,61 +3,11 @@
     <div class="buttons">
       <div>
         <div class="buttons-column">
-          <div class="column-header">
-            Normal
-          </div>
           <div>
-            <DxButton :width="120" text="Contained" type="normal" styling-mode="contained" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Outlined" type="normal" styling-mode="outlined" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" icon="check" text="Text" type="normal" styling-mode="text" @click="onClick($event)" />
-          </div>
-        </div>
-        <div class="buttons-column">
-          <div class="column-header">
-            Success
-          </div>
-          <div>
-            <DxButton :width="120" text="Contained" type="success" styling-mode="contained" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Outlined" type="success" styling-mode="outlined" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Text" type="success" styling-mode="text" @click="onClick($event)" />
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="buttons-column">
-          <div class="column-header">
-            Default
-          </div>
-          <div>
-            <DxButton :width="120" text="Contained" type="default" styling-mode="contained" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Outlined" type="default" styling-mode="outlined" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Text" type="default" styling-mode="text" @click="onClick($event)" />
-          </div>
-        </div>
-        <div class="buttons-column">
-          <div class="column-header">
-            Danger
-          </div>
-          <div>
-            <DxButton :width="120" text="Contained" type="danger" styling-mode="contained" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Outlined" type="danger" styling-mode="outlined" @click="onClick($event)" />
-          </div>
-          <div>
-            <DxButton :width="120" text="Text" type="danger" styling-mode="text" @click="onClick($event)" />
+            <DxButton :width="120" text="Text" type="normal" styling-mode="outlined" @click="onClick($event)" />
+            <DxButton :width="120" text="Text" type="success" styling-mode="outlined" @click="onClick($event)" />
+            <DxButton :width="120" text="Text" type="default" styling-mode="outlined" @click="onClick($event)" />
+            <DxButton :width="120" text="Text" type="danger" styling-mode="outlined" @click="onClick($event)" />
           </div>
         </div>
       </div>
@@ -66,7 +16,6 @@
 </template>
 
 <script lang="ts">
-
 import { Component, Ref, Prop, PropSync, Emit, Watch, Model, Inject, InjectReactive, Provide, ProvideReactive, Vue } from 'vue-property-decorator'
 import PracAppComponent from '@/components/prac-app-component'
 import { DxChart, DxSeries, DxCommonSeriesSettings, DxLegend, DxSize, DxLabel, DxFormat, DxValueAxis, DxTooltip } from 'devextreme-vue/chart'
