de5c7d5d935050192a787bb62630d0d76c9addad
oseongryu <oseongryu@gmail.com>
2023-09-06 16:45:50 +0900

    chore: devextreme setting

--- components/prac-app-component.ts
+++ components/prac-app-component.ts
@@ -1,6 +1,13 @@
-import { Component, Ref, Prop, PropSync, Emit, Watch, Model, Inject, InjectReactive, Provide, ProvideReactive, Vue } from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator'
+
+// https://js.devexpress.com/Documentation/22_2/Guide/Themes_and_Styles/Predefined_Themes/ 
+import 'devextreme/dist/css/dx.light.css'; 'devextreme/dist/css/dx.light.css';
 
 @Component
-export default class PracAppComponent extends Vue {}
+export default class PracAppComponent extends Vue {
+  created() {
+
+  }
+}
 
 export { PracAppComponent }
