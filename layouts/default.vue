06f6c6faf5278316e259d8d325bcf5774170be78
oseongryu <oseongryu@gmail.com>
2025-03-18 10:07:59 +0900

    feat: element-ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,9 @@
 <script lang="ts">
 import { Component, Ref, Prop, PropSync, Emit, Watch, Model, Inject, InjectReactive, Provide, ProvideReactive, Vue } from 'vue-property-decorator'
 import PracAppComponent from '@/components/prac-app-component'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
 
 @Component({
   components: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,7 @@
 <template>
-  <body class="hold-transition skin-red layout-top-nav">
-    <div class="wrapper">
-      <div class="content-wrapper">
-        <div class="container">
-          <!-- Content Header -->
-          <!-- <section class="content-header"></section> -->
-          <!-- Content Header -->
-          <!-- Content Body-->
-          <section class="content">
-            <div class="row">
-              <!-- Content Left -->
-              <div class="col-md-12">
-                <div class="box box-danger">
-                  <div class="box-header with-border">
-                    <h3 class="box-title"></h3>
-                    <div class="box-tools pull-right"></div>
-                  </div>
-                  <div class="box-body" style="height: 700px">
-                    <div id="drawingView" class="table-responsive" style="width: 100%; height: 100%; overflow: auto"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-          <!-- Content Body-->
-        </div>
-      </div>
-    </div>
-  </body>
+  <div>
+    <el-button @click="test" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -43,6 +17,10 @@ export default class extends PracAppComponent {
 
   mounted() {
     // this.$router.push({ path: '/guide/guide-index', append: true })
+  }
+
+  test() {
+    this.$message('This is a message.')
   }
 }
 </script>
